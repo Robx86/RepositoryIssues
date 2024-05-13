4d3d10ef1383c51151d2f84f8e7aef345e414f1b
Robson Barbosa <robsonbarbosa.dev@gmail.com>
2024-05-13 16:14:43 -0300

    fix comments on issue

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -3,6 +3,7 @@ import { useQuery } from "react-query";
 import { GoComment } from 'react-icons/go';
 import { IssueHeader } from "./IssueHeader";
 import { relativeDate } from "../helpers/relativeDate";
+import { useUserData } from "../helpers/useUserData";
 
 function useIssueData(issueNumber) {
   return useQuery(["issue", issueNumber], () => {
@@ -12,36 +13,36 @@ function useIssueData(issueNumber) {
 
 function useIssueComments(issueNumber) {
   return useQuery(["issues", issueNumber, "comments"], () => {
-    return fetch(`/api/issues/${issueNumber}/comments`).then((res) => res.json());
+    return fetch(`/api/issues/${issueNumber}/comments`).then((res) =>
+      res.json()
+    );
   });
 }
 
 function Comment({ comment, createdBy, createdDate }) {
   const userQuery = useUserData(createdBy);
-  if (userQuery.isLoading) {
-    return <div className="comment">
-      <div>
-        <div className="comment-header">
-          Loading
+
+  if (userQuery.isLoading)
+    return (
+      <div className="comment">
+        <div>
+          <div className="comment-header">Loading...</div>
         </div>
       </div>
-    </div>;
-  }
-
-  return <div className="comment">
-    <img src={userQuery.data?.avatar} alt='Commenter Avatar' />
-    <div>
-      <div className="commenter-header">
-        <span>{userQuery.data?.name}</span> commented{' '}
-        <span>{relativeDate(createdDate)}</span>
-      </div>
-      <div className="comment-body">
-        {comment}
+    );
+  return (
+    <div className="comment">
+      <img src={userQuery.data.profilePictureUrl} alt="Commenter Avatar" />
+      <div>
+        <div className="comment-header">
+          <span>{userQuery.data.name}</span> commented{" "}
+          <span>{relativeDate(createdDate)}</span>
+        </div>
+        <div className="comment-body">{comment}</div>
       </div>
     </div>
-  </div>;
+  );
 }
-
 
 export default function IssueDetails() {
   const { number } = useParams();
